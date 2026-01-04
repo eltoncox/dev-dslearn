bd5fff1602a9c7d02546c9150a7ba4d425af0847
eltoncox <elton.unix@gmail.com>
2026-01-04 12:48:51 -0300

    Entities User, Role

---
+++ src/main/java/com/dev/dslearnbds/DslearnbdsApplication.java
@@ -0,0 +1,12 @@
+package com.dev.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+}
